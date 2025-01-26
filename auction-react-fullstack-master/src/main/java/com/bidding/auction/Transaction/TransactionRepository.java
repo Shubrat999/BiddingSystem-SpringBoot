79ab5836f71381b1bcbe31634b3a79195e301dba
Subrat Setha <143266377+Shubrat999@users.noreply.github.com>
2025-01-26 20:15:00 +0530

    Delete auction-react-fullstack-master directory

--- auction-react-fullstack-master/src/main/java/com/bidding/auction/Transaction/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bidding.auction.Transaction;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository//connects to transaction table 
-public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
-    
-}
--- auction-react-fullstack-master/src/main/silentbidding/src/hos/Auxi.js
+++
@@ -1,4 +0,0 @@
-//acts as a wrapper for all components so that all elemenent belong to one element
-const Auxi=(props)=>props.children
-
-export default Auxi;
